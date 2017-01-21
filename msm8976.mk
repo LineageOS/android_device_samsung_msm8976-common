ee2ea41997e2d838413d8ee77f3aaa805ba684f7
LuK1337 <priv.luk@gmail.com>
2017-01-21 12:07:47 +0100

    msm8976-common: Add proper mixer paths

--- msm8976.mk
+++ msm8976.mk
@@ -87,7 +87,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml
+    $(LOCAL_PATH)/audio/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml
 
 # Browser
 PRODUCT_PACKAGES += \
