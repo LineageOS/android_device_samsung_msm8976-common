a33c38a0d99130679840a3c2b994492af7d0e41c
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-26 09:19:58 +0000

    msm8976-common: Remove low_latency permission
    * The latency is about 80ms *sigh*
    
    Change-Id: I89d16d24d465c951ad8a747bcd7a5671c529eec9

--- msm8976.mk
+++ msm8976.mk
@@ -36,7 +36,6 @@ $(call inherit-product-if-exists, frameworks/native/build/tablet-10in-xhdpi-2048
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
