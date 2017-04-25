1d84d1c5f4d335103d668d3b9488d6630624cd10
Luca Stefani <luca020400@lineageos.org>
2017-04-25 21:58:15 +0200

    msm8976-common: Add libcamera_client shim
    Change-Id: I4cc4eb52bc41e5174342e5571f096edb0e5a5c75

--- camera/Android.mk
+++ camera/Android.mk
@@ -33,3 +33,13 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := camera_parameters.cpp
+LOCAL_MODULE := libcamera_parameters_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
--- msm8976.mk
+++ msm8976.mk
@@ -105,7 +105,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8952
+    camera.msm8952 \
+    libcamera_parameters_shim
 
 # Display
 PRODUCT_PACKAGES += \
