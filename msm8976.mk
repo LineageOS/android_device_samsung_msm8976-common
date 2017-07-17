c193b62d1f0770cb794f452beb1c26ea1a375de2
Sean hoyt <seanhoyt963@gmail.com>
2017-07-17 10:54:22 +0000

    Move some audio files down to the device trees
    Change-Id: I3795cb5073e8c35f6ac5a72c6d21d4f316588e79

--- msm8976.mk
+++ msm8976.mk
@@ -92,7 +92,6 @@ PRODUCT_COPY_FILES += \
 
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
