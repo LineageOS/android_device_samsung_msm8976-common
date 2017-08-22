522d08df0c614a758b470bf9475a91d7bb6e1548
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    msm8976-common: Update audio for O
    * Build audio HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL

--- msm8976.mk
+++ msm8976.mk
@@ -70,18 +70,23 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
-    audio.a2dp.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
+    audio.a2dp.default \
     audio.usb.default \
+    audio.r_submix.default
+
+PRODUCT_PACKAGES += \
     libaudio-resampler \
     libaudioroute \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing \
     libtinycompress \
-    tinymix
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
