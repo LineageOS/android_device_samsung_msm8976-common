d3222902e9dced8ce73ac2ed1d6723b1b8ff6bb7
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:21 +0200

    msm8976-common: Build Gatekeeper HAL

--- msm8976.mk
+++ msm8976.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # GPS
 PRODUCT_PACKAGES += \
     libcurl \
