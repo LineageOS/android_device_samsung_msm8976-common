2807542d4bb70be4fe9e15eb0ff1a70c1b38c1ae
LuK1337 <priv.luk@gmail.com>
2017-09-20 14:21:41 +0200

    msm8976-common: /system/bin -> /system/vendor/bin

--- config.fs
+++ config.fs
@@ -7,25 +7,25 @@ value:2951
 [AID_RFS_SHARED]
 value:2952
 
-[system/bin/cnss-daemon]
+[system/vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/pm-service]
+[system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
+[system/vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
