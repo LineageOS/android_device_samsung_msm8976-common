2bf29301cce2d1247af9297de84b59ed9fa88622
LuK1337 <priv.luk@gmail.com>
2017-12-20 00:55:29 +0100

    msm8976-common: Convert audio_effects.conf to XML
    * Converted using : https://github.com/luk1337/aeffects-conf2xml

--- msm8976.mk
+++ msm8976.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
