436b2f46a9040bfa89e83a6571413d11d98e4a49
Bruno Martins <bgcngm@gmail.com>
2017-12-26 23:32:52 +0000

    msm8976-common: Move IPC router config to vendor
    Change-Id: I8d6892c8205a307f3a236f7ce8c78f9bdead2ba5

--- msm8976.mk
+++ msm8976.mk
@@ -171,7 +171,7 @@ PRODUCT_PACKAGES += \
 
 # IPC router config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # IPv6
 PRODUCT_PACKAGES += \
