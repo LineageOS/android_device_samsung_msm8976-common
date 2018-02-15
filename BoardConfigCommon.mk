d3127643bbe1e79a5d224365591db4babb9cae93
Paul Keith <javelinanddart@gmail.com>
2018-02-15 23:33:28 +0100

    msm8976-common: Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
     * As a bonus, we no longer need noatsecure
       to make LD_SHIM_LIBS persist through services
    
    Change-Id: I877192422062f3e59c81a7130ad1a2b4be5d1647

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,6 +206,9 @@ include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
+# Shims
+TARGET_LD_SHIM_LIBS := /system/lib/libcamera_client.so|libcamera_parameters_shim.so:/system/vendor/lib64/libizat_core.so|libshims_get_process_name.so:/system/lib64/libsec-ril.so|libshims_rild_socket.so
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
