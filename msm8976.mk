604a950fd3820bb60274d4f121f58765a58980fc
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    msm8976-common: Remove non-existing HAL service
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- msm8976.mk
+++ msm8976.mk
@@ -93,8 +93,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
