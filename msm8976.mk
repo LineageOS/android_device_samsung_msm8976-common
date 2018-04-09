b15744fd7b47605724dc93e6723a8689491020bc
LuK1337 <priv.luk@gmail.com>
2018-04-10 01:08:06 +0200

    msm8976-common: Go back passthrough sensors implementation
    * Apparently hwbinder causes issues with adaptive
      brightness. Going back to passthrough seems to fix
      this issue.
    
    Change-Id: I47d60d2d7fb52b5001df8a0d385acddebaa0ec84

--- msm8976.mk
+++ msm8976.mk
@@ -269,8 +269,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # TimeKeep
 PRODUCT_PACKAGES += \
