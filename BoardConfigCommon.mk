43da7dfa4c982657daa5ecbf0b84994f19d86752
LuK1337 <priv.luk@gmail.com>
2018-08-12 11:14:29 +0200

    msm8976-common: Disable our and QCOM sepolicy
    Change-Id: Id6d617ead095ef4a0d37e0d8ee1996cd7f0b10bc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,9 +213,9 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_samsung
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+# include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/legacy-sepolicy.mk
+# BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
