00999089cedc5b531a4a630ac8f2c306cb8a608d
LuK1337 <priv.luk@gmail.com>
2018-08-12 20:51:09 +0200

    msm8976-common: Fix build of librecovery_updater_samsung
    Change-Id: I308fca4987b049266ebaae04de8b3e0efbf836ba

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
+    $(call project-path-for,recovery)/edify/include \
+    $(call project-path-for,recovery)/otautil/include \
     $(call project-path-for,recovery)/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_samsung
