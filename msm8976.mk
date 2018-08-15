dcd5e38993a67e4d98f9a991c0966f1b11874d9d
LuK1337 <priv.luk@gmail.com>
2018-08-16 01:09:37 +0200

    msm8976-common: Build cool USB hal
    * No that is not a typo, it's just me being lazy.
    
    Change-Id: I2a3d3ea205d821ec60b33ccc9cc40c077d43bd47

--- msm8976.mk
+++ msm8976.mk
@@ -269,7 +269,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.xiaomi_8996
 
 # WCNSS
 PRODUCT_COPY_FILES += \
