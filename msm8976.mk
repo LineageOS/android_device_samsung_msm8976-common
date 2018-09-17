ced29a900f9b1790323ed32135f9700305226476
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    msm8976-common: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- msm8976.mk
+++ msm8976.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
