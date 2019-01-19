8ae15d9d5dc162cca89a6732a05f8a62b52bbb2a
LuK1337 <priv.luk@gmail.com>
2019-01-19 12:48:23 +0100

    msm8976-common: Switch to regular camera provider impl
    Change-Id: If6dd0c24b20e8520c01e72b83a8bc623d14ff518

--- msm8976.mk
+++ msm8976.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
     libshims_camera
