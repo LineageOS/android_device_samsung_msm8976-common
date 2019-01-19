3087dd8f087f0b2a4a19f37e826673bdad7578ae
LuK1337 <priv.luk@gmail.com>
2019-01-19 12:22:46 +0100

    Revert "msm8976-common: Binderize camera & graphics composer"
    * Since someone was funny enough to label display.qservice
      as 'service' instead of 'vndservice' in legacy qcom sepolicy
      I'm forced to move these back to passthrough ;-;
    
    This reverts commit a8fd5e61e063b976d6e647fe528334ecfa4126f7.
    
    Change-Id: I4fb729fce584450499b55cc7b8815e9768dac846

--- msm8976.mk
+++ msm8976.mk
@@ -119,7 +119,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
     libshims_camera
 
@@ -133,7 +132,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
