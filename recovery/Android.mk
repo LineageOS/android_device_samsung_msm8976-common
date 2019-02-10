a93c852e377363ffa83e070f182e734d2619949d
LuK1337 <priv.luk@gmail.com>
2019-02-10 12:13:49 +0100

    msm8976-common: recovery: Drop pathmap dependency
    * It's unlikely to be merged anyway.
    
    Change-Id: Id6c968ff54d4dc51a45a1f04fe64cade2ae3c464

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/edify/include \
-    $(call project-path-for,recovery)/otautil/include \
-    $(call project-path-for,recovery)/updater/include
+LOCAL_C_INCLUDES :=
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_samsung
 LOCAL_MODULE_TAGS := eng
