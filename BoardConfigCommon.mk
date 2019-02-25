38786d49b828e9087a0d33d28c36ab36d842dfaa
LuK1337 <priv.luk@gmail.com>
2019-02-25 16:52:14 +0100

    Revert "Revert "msm8976-common: Binderize camera & graphics composer""
    This reverts commit f10851da4f6cd4e6144bf2db4b83c606a038686b.
    
    Change-Id: Ia054b3433e8bd3652ad6557180e739a0ea5bd49c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,7 +104,7 @@ TARGET_BOARD_INFO_FILE := $(VENDOR_PATH)/board-info.txt
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /system/bin/cameraserver=24
+    /system/vendor/bin/hw/android.hardware.camera.provider@2.4-service=24
 TARGET_USES_MEDIA_EXTENSIONS := true
 
 # Charger
